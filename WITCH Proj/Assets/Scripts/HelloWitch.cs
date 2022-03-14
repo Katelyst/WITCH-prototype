5eae000b6cbef6149d04076789b4614947010d11
Jochem <joaghim.beltman@gmail.com>
2022-03-14 21:15:50 +0100

    Added starter assets & prefab test for source control

---
+++ WITCH Proj/Assets/Scripts/HelloWitch.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Debug.Log("Hello Witch, if you see this everything *should* be g00d to go");
+    }
+
+}
