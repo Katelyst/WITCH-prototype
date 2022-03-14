d30eb6b6db50013ea04806f19671eddda9c7a2f9
Sebastiaan <wiggler@live.nl>
2022-03-14 23:46:41 +0100

    Hello Witch github test
    addded comment to test stufful

--- WITCH Proj/Assets/Scripts/HelloWitch.cs
+++ WITCH Proj/Assets/Scripts/HelloWitch.cs
@@ -8,6 +8,7 @@ public class HelloWitch : MonoBehaviour
     void Awake()
     {
         Debug.Log("Hello Witch, if you see this everything *should* be g00d to go");
+        //This is an otter that lives in your code. please remove me :3!!!!
     }
 
 }
